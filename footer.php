916b5a47a4c120065d86bc1313bb9a85b173e641
gabskir <gabriele.skirm@gmail.com>
2024-06-22 19:18:32 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <p>&copy; 2024 Scientific Conference</p>
+        <div class="social-media">
+            <a href="#"><img src="imgs/facebook.png" alt="Facebook"></a>
+            <a href="#"><img src="imgs/twitter.png" alt="Twitter"></a>
+            <a href="#"><img src="imgs/linkedin.png" alt="LinkedIn"></a>
+        </div>
+    </footer>
+    <div id="article-popup" class="popup">
+        <div class="popup-content">
+            <span class="close" onclick="closePopup()">&times;</span>
+            <div id="article-details"></div>
+        </div>
+    </div>
+</body>
+</html>
